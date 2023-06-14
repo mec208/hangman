bf74f2f7b18b1ab61f79d84aef3b3c881af8a706
Matt Cairns <matt_cairns@outlook.com>
2023-06-14 20:06:29 +0100

    update milestone_2.py to include random module

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,11 @@
+import random
+
 word_list = ["apple", "banana", "orange", "mango", "pineapple"]
 print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+# for i in range(5):
+#     word = random.choice(word_list)
+#     print(word)
